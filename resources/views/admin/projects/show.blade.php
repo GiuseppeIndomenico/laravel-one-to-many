e6b3145829af5262ed321497790f13fd3c21765c
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-06-08 19:32:03 +0200

    fix

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -6,7 +6,10 @@
         <h1 class="display-1 text-center fw-semibold mb-3"> {{ $project->title }}</h1>
         <a class="btn btn-outline-warning" href="{{ route('admin.projects.edit', $project->slug) }}"> <i
                 class="fa-solid fa-pencil"></i> Modifica</a>
-        <p class="py-4">{{ $project->description }}</p>
+        <p class="py-4"> <span class="fw-bold"> Nome del progetto: </span> {{ $project->description }}</p>
+        <p><span class="fw-bold">tipologia di progetto:</span> {{ $project->type->name }}</p>
+
+
 
 
     </div>
